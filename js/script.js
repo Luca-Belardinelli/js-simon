12ae8fa5ffd0ade8b1e3061e46b2ea7b1894a63f
Luca Belardinelli <belardinelliluca@gmail.com>
2025-01-15 11:47:56 +0100

    [js_2]

--- js/script.js
+++ js/script.js
@@ -25,5 +25,50 @@ let timer = 5;
 //elemente in cui vedere il timer in pagina
 let countdown = document.getElementById('countdown');
 
-// inseriamo il timer in pagina
+// funzione
+let conteggio = setInterval(() => {
 
+    // stampa il timer in pagina
+    countdown.innerHTML = timer;
+
+    // decrementa il timer
+    timer--;
+
+    // ferma il contatore quando arriva a 0
+    if (timer < 0) {
+        clearInterval(conteggio);
+    
+
+    //rimuovi contenuto ul impostandolo a stringa vuota
+    ul.innerHTML = '';
+
+    //fai apparire il form rimuovendo la classe d-none di bootstrap
+    document.getElementById('answers-form').classList.remove('d-none');
+
+ }
+
+},1000);
+
+
+
+// inserire i  5 numeri
+const fiveNumber = [];
+
+//seleziona l'elemento form
+document.getElementById('answers-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    //seleziona  gli input del form
+    const inputs = document.querySelectorAll('#answers-form input');
+
+    //popola l'array con i valori degli input convertiti in numeri interi
+    inputs.forEach(input => {
+        fiveNumber.push(parseInt(input.value));
+    });
+
+    //verifica quanti numeri  combaciano
+    const correctNumbers = numbers.filter((number) => fiveNumber.includes(number));
+
+    //stampa in pagina  il risultato
+    document.getElementById('message').innerHTML = `hai indovinato ${correctNumbers.length} numeri: ${correctNumbers.join(', ')}`;
+});
